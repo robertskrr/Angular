f542b60aa41eb9c1b95ee31acf5ebd991f04a8de
Robert Esquerre Valiente <robertesquerre24@gmail.com>
2026-09-21 12:01:16 +0200

    feat: functions with objects

--- 01-typescript-intro/src/topics/03-functions.ts
+++ 01-typescript-intro/src/topics/03-functions.ts
@@ -14,9 +14,35 @@ function multiply(
   return firstNumber * base;
 }
 
+/*
 const result: number = addNumbers(1, 2);
 const result2: string = addNumbersArrow(1, 2);
 const multiplyResult: number = multiply(5);
 
 console.log({ result, result2, multiplyResult });
+*/
+
+interface Character {
+  name: string;
+  hp: number;
+  showHp: () => void;
+}
+
+const healCharacter = (character: Character, amount: number) => {
+  character.hp += amount;
+};
+
+const strider: Character = {
+  name: "Strider",
+  hp: 50,
+  showHp() {
+    console.log(`Puntos de vida ${this.hp}`);
+  },
+};
+
+healCharacter(strider, 10);
+healCharacter(strider, 50);
+
+strider.showHp();
+
 export {};
