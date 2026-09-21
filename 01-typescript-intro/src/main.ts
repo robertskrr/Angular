3a8ef644c8317cb4f88dc6b942f65f0a92dbcb77
Robert Esquerre Valiente <robertesquerre24@gmail.com>
2026-09-21 11:07:50 +0200

    feat: Added typescript intro

---
+++ 01-typescript-intro/src/main.ts
@@ -0,0 +1,9 @@
+import './style.css'
+
+//import './topics/01-basic-types';
+
+const app = document.querySelector<HTMLDivElement>('#app')!;
+
+app.innerHTML = 'Hola Mundo';
+
+console.log('Buenas');
