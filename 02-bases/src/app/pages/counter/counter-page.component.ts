5d79ad1ebb89d3a0d2c44232bd251d145f5b5f6e
Robert Esquerre Valiente <robertesquerre24@gmail.com>
2026-09-23 18:00:32 +0200

    feat: hero page component

--- 02-bases/src/app/pages/counter/counter-page.component.ts
+++ 02-bases/src/app/pages/counter/counter-page.component.ts
@@ -7,12 +7,14 @@ import { Component, signal } from '@angular/core';
       padding: 5px;
       margin: 5px 10px;
       width: 75px;
-      }
+    }
   `,
 })
 export class CounterPageComponent {
   counter = 10;
   counterSignal = signal(10);
+
+  constructor() {}
 
   increaseBy(value: number) {
     this.counter += value;
@@ -20,7 +22,7 @@ export class CounterPageComponent {
   }
 
   resetCounter() {
-    this.counter = 0
+    this.counter = 0;
     this.counterSignal.set(0);
   }
 }
