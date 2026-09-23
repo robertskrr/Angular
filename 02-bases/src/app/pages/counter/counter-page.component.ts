a8f44adc6fa38bc91234b8b101d81c70f66fc9ee
Robert Esquerre Valiente <robertesquerre24@gmail.com>
2026-09-23 14:50:17 +0200

    feat: counter page component

---
+++ 02-bases/src/app/pages/counter/counter-page.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './counter-page.component.html',
+  styles: `
+    button {
+      padding: 5px;
+      margin: 5px 10px;
+      width: 75px;
+      }
+  `,
+})
+export class CounterPageComponent {
+  counter = 10;
+
+  increaseBy(value: number) {
+    this.counter += value;
+  }
+
+  resetCounter() {
+    this.counter = 10;
+  }
+}
