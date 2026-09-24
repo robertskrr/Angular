88b273b9850917c1af943fe527328e1a3888043d
Robert Esquerre Valiente <robertesquerre24@gmail.com>
2026-09-24 13:02:43 +0200

    feat: dragon ball list and inputs

--- 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -10,15 +10,39 @@ interface Character {
   templateUrl: './dragonball-page.component.html',
 })
 export class DragonballPageComponent {
+  name = signal('Gohan');
+  power = signal(100);
+
   characters = signal<Character[]>([
-    {id:1, name: 'Goku', power: 9001},
-    {id:2, name: 'Vegeta', power: 8000},
-    {id:3, name: 'Piccolo', power: 3000},
+    { id: 1, name: 'Goku', power: 9001 },
+    { id: 2, name: 'Vegeta', power: 8000 },
+    { id: 4, name: 'Yamcha', power: 500 },
+    { id: 3, name: 'Piccolo', power: 3000 },
   ]);
 
   powerClasses = computed(() => {
     return {
       'text-danger': true,
+    };
+  });
+
+  addCharacter() {
+    if (!this.name() || !this.power() || this.power() <= 0) {
+      return;
     }
-  })
+
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power(),
+    };
+
+    this.characters.update((list) => [...list, newCharacter]);
+    this.resetFields();
+  }
+
+  resetFields() {
+    this.name.set('');
+    this.power.set(0);
+  }
 }
