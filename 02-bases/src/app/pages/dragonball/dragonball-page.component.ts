09982d8303442e3f45af2c6ca671ba6c95088a37
Robert Esquerre Valiente <robertesquerre24@gmail.com>
2026-09-24 11:32:09 +0200

    feat: dragon ball page

---
+++ 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -0,0 +1,24 @@
+import { Component, computed, signal } from '@angular/core';
+
+interface Character {
+  id: number;
+  name: string;
+  power: number;
+}
+
+@Component({
+  templateUrl: './dragonball-page.component.html',
+})
+export class DragonballPageComponent {
+  characters = signal<Character[]>([
+    {id:1, name: 'Goku', power: 9001},
+    {id:2, name: 'Vegeta', power: 8000},
+    {id:3, name: 'Piccolo', power: 3000},
+  ]);
+
+  powerClasses = computed(() => {
+    return {
+      'text-danger': true,
+    }
+  })
+}
