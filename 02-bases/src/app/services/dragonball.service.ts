d57f8801fc2a9368efe24d4496558775d3873a29
Robert Esquerre Valiente <robertesquerre24@gmail.com>
2026-09-25 12:14:21 +0200

    feat: effects in localStorage

--- 02-bases/src/app/services/dragonball.service.ts
+++ 02-bases/src/app/services/dragonball.service.ts
@@ -1,13 +1,20 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+
+const loadFromLocalStorage = (): Character[] => {
+  const characters = localStorage.getItem('characters');
+
+  return characters ? JSON.parse(characters) : [];
+};
 
 @Injectable({ providedIn: 'root' })
 export class DragonballService {
+  characters = signal<Character[]>(loadFromLocalStorage());
 
-  characters = signal<Character[]>([
-    { id: 1, name: 'Goku', power: 9001 },
-    { id: 2, name: 'Vegeta', power: 8000 },
-  ]);
+  // Guarda los personajes en localStorage
+  saveToLocalStorage = effect(() => {
+    localStorage.setItem('characters', JSON.stringify(this.characters()));
+  });
 
   addCharacter(character: Character) {
     this.characters.update((list) => [...list, character]);
