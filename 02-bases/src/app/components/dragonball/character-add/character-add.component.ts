a09d39e2b8b0d1238df42fb8a31398c8f17c30d6
Robert Esquerre Valiente <robertesquerre24@gmail.com>
2026-09-25 11:07:01 +0200

    feat: output and services

--- 02-bases/src/app/components/dragonball/character-add/character-add.component.ts
+++ 02-bases/src/app/components/dragonball/character-add/character-add.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, output, signal } from '@angular/core';
 import { Character } from '../../../interfaces/character.interface';
 
 @Component({
@@ -9,20 +9,19 @@ export class CharacterAddComponent {
   name = signal('');
   power = signal(0);
 
+  newCharacter = output<Character>();
   addCharacter() {
     if (!this.name() || !this.power() || this.power() <= 0) {
       return;
     }
 
     const newCharacter: Character = {
-      //id: this.characters().length + 1,
-      id: 10000,
+      id: Math.floor(Math.random() * 1000),
       name: this.name(),
       power: this.power(),
     };
 
-    //this.characters.update((list) => [...list, newCharacter]);
-    console.log({ newCharacter });
+    this.newCharacter.emit(newCharacter);
     this.resetFields();
   }
 
--- 02-bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ 02-bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CharacterListComponent } from '../../components/dragonball/character-list/character-list.component';
 import { CharacterAddComponent } from '../../components/dragonball/character-add/character-add.component';
+import { DragonballService } from '../../services/dragonball.service';
 
 interface Character {
   id: number;
@@ -14,8 +15,6 @@ interface Character {
   imports: [CharacterListComponent, CharacterAddComponent],
 })
 export class DragonballSuperPageComponent {
-  characters = signal<Character[]>([
-    { id: 1, name: 'Goku', power: 9001 },
-    { id: 2, name: 'Vegeta', power: 8000 },
-  ]);
+  public dragonballService = inject(DragonballService);
+
 }
